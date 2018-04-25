cce191b4c9a26f3c16ed192a4505bb530e9f0074
RomarioGiacholari <giacholari@gmail.com>
2018-04-25 02:24:01 +0100

    added text when there is no event to list and the back button

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -9,24 +9,30 @@
         </div>
         <hr />
         <div class="row">
-            @foreach($events as $event)
-            <div class="col-md-4 p-3">
-                <a href="{{ $event->path() }}">
-                    <div class="card bg-dark text-white" style="font-family:Comic Sans MS, cursive, sans-serif">
-                        <img class="card-img" src="{{ url('/photos/' .$event->thumbnail_path) }}" style="opacity:0.2;">
-                        <!-- <img class="card-img" src="{{ $event->thumbnail_path }}" style="opacity:0.2;"> -->
-                        <div class="card-img-overlay  event-thumbnail">
-                            <h5 class="card-title">title: {{ $event->name }}</h5>
-                            <p class="card-text"> venue: {{ $event->venue }}</p>
-                            <p class="card-text">type: {{ $event->type }}</p>
-                            <p class="card-text">likes: {{ $event->favorites_count }}</p>
-                            <p class="card-text">due date: {{ \Carbon\Carbon::parse($event->due_date)->diffForHumans() }}</p>
+            @if(count($events))
+                @foreach($events as $event)
+                <div class="col-md-4 p-3">
+                    <a href="{{ $event->path() }}">
+                        <div class="card bg-dark text-white" style="font-family:Comic Sans MS, cursive, sans-serif">
+                            <img class="card-img" src="{{ url('/photos/' .$event->thumbnail_path) }}" style="opacity:0.2;">
+                            <!-- <img class="card-img" src="{{ $event->thumbnail_path }}" style="opacity:0.2;"> -->
+                            <div class="card-img-overlay  event-thumbnail">
+                                <h5 class="card-title">title: {{ $event->name }}</h5>
+                                <p class="card-text"> venue: {{ $event->venue }}</p>
+                                <p class="card-text">type: {{ $event->type }}</p>
+                                <p class="card-text">likes: {{ $event->favorites_count }}</p>
+                                <p class="card-text">due date: {{ \Carbon\Carbon::parse($event->due_date)->diffForHumans() }}</p>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-
-            @endforeach 
+                    </a>
+                </div>
+                @endforeach 
+            @else
+                <div class="col-md-4 p-3">
+                    <p>No events.</p>
+                    <a href="{{ url()->previous() }}" class="btn btn-outline-success">back</a>
+                </div>
+            @endif
 
         </div>
 
